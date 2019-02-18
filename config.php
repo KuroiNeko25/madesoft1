6f74e73f1c883539fc116086d45b2bb48335d45c
Diana Lozano <diana.patricia.lozano@correounivalle.edu.co>
2019-02-17 20:57:10 -0500

    Subida inicial
    Todos los archivos locales iniciales

---
+++ config.php
@@ -0,0 +1,36 @@
+<?php
+	$dbServer = 'localhost';
+	$dbUsername = 'dipalaof_dianap';
+	$dbPassword = 'm4d3s0ft1';
+	$dbDatabase = 'dipalaof_inventory';
+
+	$adminConfig = array(
+		'adminUsername' => "administrador",
+		'adminPassword' => "1bdc4e99d504cd17d7f1f551ee56fe3c",
+		'notifyAdminNewMembers' => "",
+		'defaultSignUp' => "1",
+		'anonymousGroup' => "anonymous",
+		'anonymousMember' => "guest",
+		'groupsPerPage' => "10",
+		'membersPerPage' => "10",
+		'recordsPerPage' => "10",
+		'custom1' => "Full Name",
+		'custom2' => "Address",
+		'custom3' => "City",
+		'custom4' => "State",
+		'MySQLDateFormat' => "%d/%m/%Y",
+		'PHPDateFormat' => "j/n/Y",
+		'PHPDateTimeFormat' => "d/m/Y, h:i a",
+		'senderName' => "Membership management",
+		'senderEmail' => "dipaloz@gmail.com",
+		'approvalSubject' => "Your membership is now approved",
+		'approvalMessage' => "Dear member,\n\nYour membership is now approved by the admin. You can log in to your account here:\nhttp://localhost/inventory\n\nRegards,\nAdmin",
+		'hide_twitter_feed' => "",
+		'maintenance_mode_message' => "<b>Our website is currently down for maintenance</b><br>\r\nWe expect to be back in a couple hours. Thanks for your patience.",
+		'mail_function' => "mail",
+		'smtp_server' => "",
+		'smtp_encryption' => "",
+		'smtp_port' => "25",
+		'smtp_user' => "",
+		'smtp_pass' => ""
+	);
