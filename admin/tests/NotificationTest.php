bd670dc541ea15f33e4f74c075cd658dab3e09df
Diana Lozano <diana.patricia.lozano@correounivalle.edu.co>
2019-05-17 09:24:26 -0500

    cambio ubicacion de prueba
    hacia la carpeta admin/tests

---
+++ admin/tests/NotificationTest.php
@@ -0,0 +1,14 @@
+<?php
+use PHPUnit\Framework\TestCase;
+use Notification;
+
+final class NotificationTest extends TestCase{
+	public function testSendmail(){
+		$miObj = new Notification();
+		
+		$correo = [ "subject" => "asunto", "message" => "hola", "name" => "Diana", "debug" => "nada",];
+		
+		$this->assertEquals(true, $miObj->sendmail($correo));
+	}
+
+}
