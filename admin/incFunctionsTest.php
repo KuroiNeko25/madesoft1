a61daaffd2be8112ef53406c8e9a5e4e53ba2720
dilozano <diana.patricia.lozano@correounivalle.edu.co>
2019-05-17 16:36:30 -0500

    incluye lib

--- admin/incFunctionsTest.php
+++ admin/incFunctionsTest.php
@@ -2,13 +2,14 @@
 
 <?php
 $currDir=dirname(__FILE__);
+include("$currDir/../lib.php");
 include("$currDir/incFunctions.php");
 use PHPUnit\Framework\TestCase;
 //use Notification;
 
 final class NotificationTest extends TestCase{
 	public function testSendmail(){
-		$adminConfig = config('adminConfig');
+
 		$correo = [ "to" => "dipaloz@gmail.com", "name" => "Diana", "subject" => "asunto", "message" => "hola",  "debug" => "nada", ];
 		
 		$this->assertEquals(true, sendmail($correo));
